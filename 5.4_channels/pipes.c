2b86b336fde7dcfaa3854c5d414aeb555f687e43
andreyp74 <petrov.andrey@gmail.com>
2016-02-07 21:35:35 +0300

    pipes 2nd revision

--- 5.4_channels/pipes.c
+++ 5.4_channels/pipes.c
@@ -1,42 +1,43 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void dup_and_close(int from, int to) {
-	close(to);
-	dup2(from, to);
-	close(from);
+void exec_process(const char* args) {
+	//printf("execute process: %s\n", args);
+	char* file = strtok(args, " ");
+	if (file != NULL) {
+		char* arg = strtok(NULL, " ");
+		if (arg != NULL) {
+			//printf("execlp(%s, %s, %s, NULL)\n", file, file, arg);
+			execlp(file, file, arg, NULL);
+		} else {
+			//printf("execlp(%s, %s, NULL)\n", file, file);
+			execlp(file, file, NULL);
+		}
+	}
 }
 
-void create_pipe(const char* from, const char* to) {
-	printf("pipe: %s -> %s\n", from==NULL ? "NULL" : from, to==NULL ? "NULL":to);
-	if (to == NULL) {
-		return;
-	}
+void pipe_from_to(const char* from, const char* to) {
+	//printf("pipe: %s -> %s\n", from==NULL ? "NULL" : from, to==NULL ? "NULL":to);
 	int fd[2];
 	if (pipe(fd) == -1) {
 		printf("pipe create error\n");
 		return;
 	}
-	if (!fork()) {
-		dup_and_close(fd[0], STDIN_FILENO);
-		exec_process(to);
-	} else if (from != NULL) {
-		dup_and_close(fd[1], STDOUT_FILENO);
-	}
-}
-
-void exec_process(const char* args) {
-	printf("parse args: %s\n", args);
-	char* file;
-	file = strtok(args, " ");
-	if (file != NULL) {
-		char* arg = strtok(NULL, " ");
-		if (arg != NULL) {
-			printf("execlp(%s, %s, %s, NULL)\n", file, file, arg);
-			execlp(file, file, arg, NULL);
-		} else {
-			printf("execlp(%s, %s, NULL)\n", file, file);
-			execlp(file, file, NULL);
+	if (fork()) {
+	    if (from != NULL) {
+	        close(STDOUT_FILENO);
+		    dup2(fd[1], STDOUT_FILENO);
+		    close(fd[1]);
+		    close(fd[0]);
+		    exec_process(from);
+		}
+	} else {	
+	    if (to != NULL) {
+	        close(STDIN_FILENO);
+		    dup2(fd[0], STDIN_FILENO);
+		    close(fd[1]);
+		    close(fd[0]);
+		    exec_process(to);
 		}
 	}
 }
@@ -46,10 +47,12 @@ void parse_cmd(const char* cmd) {
 	char* ch = strtok(cmd, "|");
 	char* prev = NULL;
 	while (ch != NULL) {		
-		create_pipe(prev, ch);	
-		prev = ch;
-		ch = strtok(NULL, "|");
-	}
+	    prev = ch;
+	    ch = strtok(NULL, "|");
+        if (ch != NULL) {
+            pipe_from_to(prev, ch);
+        }
+    }	
 }
 
 
@@ -57,7 +60,6 @@ int main(int argc, char** argv) {
 	if (argc < 1) {
 		return 1;
 	}
-	printf("try to run cmd: %s\n", argv[1]);
 	parse_cmd(argv[1]);
 	return 0;
 }
